f1fc7382eb24fce23917a76d92413d4fc9b7da72
FD155 <doronzo.f@live.fr>
2021-11-15 14:38:23 +0100

    up

--- R/Gradient.R
+++ R/Gradient.R
@@ -1,3 +1,13 @@
+#' Gradient Function
+#'
+#' 
+#' @param x matrice des variables explicatives
+#' @param y variable cible à prédire
+#' @param theta coefficiant de la regression logistique
+#' 
+#' @export
+#' @return Retourne un vecteur de gradient
+#'
 gradient <-
 function(x, y, theta) {
   sig = sigmoid(x%*%t(theta))
--- R/Sigmoid.R
+++ R/Sigmoid.R
@@ -1,3 +1,10 @@
+#' Sigmoid Function
+#'
+#' @param x Variable a predire
+#'
+#' @export
+#' @return Retourne les probabilités d'appartenance aux classe positives/négatives
+#'
 sigmoid <-
 function(x){
   return(1/(1+exp(-x)))
