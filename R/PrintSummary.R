d4c19491c40ea709c1b7b4907a863e0e35f75c23
FD155 <doronzo.f@live.fr>
2021-11-27 19:44:09 +0100

    up

---
+++ R/PrintSummary.R
@@ -0,0 +1,17 @@
+# Définition of our print method
+print.fit <- function(objet){
+  # Improved display
+  cat("coef = ", objet$coef, "\n", "\n")
+  cat("loss_history = ",objet$loss_history,"\n", "\n")
+  cat("nb_iter_while = ",objet$nb_iter_while,"\n", "\n")
+  cat("modalité positive = ",objet$cible,"\n", "\n")
+}
+
+
+# Définition of our summary method
+summary.fit <- function(objet){
+  # Improved display
+  cat("Call ", objet$formula, "\n", "\n")
+  cat("Coefficients : ", "\n", objet$coef, "\n", "\n")
+  cat("Number of iterations : ", "\n", objet$nb_iter_while, "\n")
+}
