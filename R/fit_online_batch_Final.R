2f303825d4bddc49bf27bd541a6f58ae5d4dd7de
GwladysKerhoas <73121667+GwladysKerhoas@users.noreply.github.com>
2021-11-29 09:33:07 +0100

    Update fit_online_batch_Final.R

--- R/fit_online_batch_Final.R
+++ R/fit_online_batch_Final.R
@@ -187,7 +187,37 @@ fit <- function(formula, data, mode, batch_size, ncores=0,coef,max_iter=100,lear
 }
 
 
+# Définition of our print method
+#' Print Overload
+#'
+#' @param objet the S3 object that the fit function returns
+#'
+#' @return Print all the objects of the instance
+#' @export
+#'
+print.fit <- function(objet){
+  # Improved display
+  cat("coef = ", objet$coef, "\n", "\n")
+  cat("loss_history = ",objet$loss_history,"\n", "\n")
+  cat("nb_iter_while = ",objet$nb_iter_while,"\n", "\n")
+  cat("modalité positive = ",objet$cible,"\n", "\n")
+}
 
+
+# Définition of our summary method
+#' Summary Overload
+#'
+#' @param objet the S3 object that the fit function returns
+#'
+#' @return Print the trained model, the final coefficients and the number of iterations executed.
+#' @export
+#'
+summary.fit <- function(objet){
+  # Improved display
+  cat("Call ",Reduce(paste, deparse(objet$formula)),"\n")
+  cat("Coefficients : ", "\n", objet$coef, "\n", "\n")
+  cat("Number of iterations : ", "\n", objet$nb_iter_while, "\n")
+}
 
 
 
